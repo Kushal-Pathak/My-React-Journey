dda0e80a95b07fea0484a76b99e6822f5973f7b1
Kushal Pathak <kushalpathak80@gmail.com>
2023-03-18 23:28:41 +0545

    First commit

---
+++ src/components/Job.js
@@ -0,0 +1,9 @@
+export default function Job(props) {
+  return (
+    <div>
+      <h1>Salary: {props.salary}</h1>
+      <h1>Position: {props.position}</h1>
+      <h1>Company: {props.company}</h1>
+    </div>
+  );
+}
