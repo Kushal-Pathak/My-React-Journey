3f9408ad5978ee57c1c041e4286ad5a398b94665
Kushal Pathak <kushalpathak80@gmail.com>
2023-03-19 17:22:01 +0545

    edited return in planet function

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,9 +1,10 @@
 export default function Planet({ planets }) {
   return (
     <div>
-      {planets.map((item, index) => {
-        if (item.isGasPlanet) return <h1 key={index}>{item.name}</h1>;
-      })}
+      {planets.map(
+        (planet, index) =>
+          planet.isGasPlanet && <h1 key={index}>{planet.name}</h1>
+      )}
     </div>
   );
 }
