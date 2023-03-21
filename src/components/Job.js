a3bf8fa2e1e094b510c652f867766dcd948fe789
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2023-03-21 23:11:57 +0545

    Delete src directory

--- src/components/Job.js
+++
@@ -1,9 +0,0 @@
-export default function Job(props) {
-  return (
-    <div>
-      <h1>Salary: {props.salary}</h1>
-      <h1>Position: {props.position}</h1>
-      <h1>Company: {props.company}</h1>
-    </div>
-  );
-}
