716e6650fb4821a3efa07c51685684808eb809c4
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2023-03-22 22:52:44 +0545

    useEffect hook basics...2

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import Job from "./components/Job";
 import Planet from "./components/Planet";
+import Text from "./components/Text";
 function App() {
+  const [showText, setShowText] = useState(false);
   return (
     <div className="App">
-      <Counter></Counter>
+      <button
+        onClick={(event) => {
+          setShowText(!showText);
+        }}
+      >
+        Show
+      </button>
+      {showText && <Text />}
     </div>
   );
 }
